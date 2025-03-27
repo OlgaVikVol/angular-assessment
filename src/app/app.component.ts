18cc6fab77b12943ccb3d6ecf3324cb60d32e94a
Olga Volobueva <olvik1605@gmail.com>
2025-03-27 11:51:58 -0400

    update: add conditional content for Tasks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,7 @@ import { TasksComponent } from './components/tasks/tasks.component';
 export class AppComponent {
   title = 'angular-assessment';
   users = DUMMY_USERS;
-  selectedUserId = 'u1';
+  selectedUserId?: string;
 
   get selectedUser() {
     return this.users.find((user) => user.id === this.selectedUserId);
