f7e44bde27271e6204ea372fb264bea3a355a015
Olga Volobueva <olvik1605@gmail.com>
2025-03-30 21:31:14 -0400

    feat: save to localStorage tasks

--- src/app/components/tasks/tasks.service.ts
+++ src/app/components/tasks/tasks.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { NewTaskData } from '../new-task/new-task.model';
 
-@Injectable({providedIn: "root"})
+@Injectable({ providedIn: 'root' })
 export class TasksService {
   private tasks = [
     {
@@ -27,6 +27,14 @@ export class TasksService {
     },
   ];
 
+  constructor() {
+    const tasks = localStorage.getItem('tasks');
+
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
+
   getUserTasks(userId: string) {
     return this.tasks.filter((task) => task.userId === userId);
   }
@@ -39,9 +47,15 @@ export class TasksService {
       dueDate: taskData.date,
       userId: userId,
     });
+    this.saveTasks();
   }
 
-  removeTask (id: string) {
+  removeTask(id: string) {
     this.tasks = this.tasks.filter((task) => task.id !== id);
+    this.saveTasks();
+  }
+
+  private saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }
